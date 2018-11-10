7ba98de62fd7bf3ffdb6b78c0bd88801d66c1755
Chris Welter <xryswelter@gmail.com>
2018-11-10 15:43:34 -0800

    updated randomization process

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -13,142 +13,140 @@ let value2 = 0;
 
 // Image Variables
 let birthstones = [
-    "./assets/images/Garnet-January.png",
-    "./assets/images/Amethyst-February.png",
-    "./assets/images/Aquamarine-March.png",
-    "./assets/images/Diamond-April.png",
-    "./assets/images/Emerald-May.png",
-    "./assets/images/Alexandrite-June.png",
-    "./assets/images/Ruby-July.png",
-    "./assets/images/Peridot-August.png",
-    "./assets/images/Sapphire-September.png",
-    "./assets/images/Opal-October.png",
-    "./assets/images/Citrine-November.png",
-    "./assets/images/Tanzanite-December.png"
+  "./assets/images/Garnet-January.png",
+  "./assets/images/Amethyst-February.png",
+  "./assets/images/Aquamarine-March.png",
+  "./assets/images/Diamond-April.png",
+  "./assets/images/Emerald-May.png",
+  "./assets/images/Alexandrite-June.png",
+  "./assets/images/Ruby-July.png",
+  "./assets/images/Peridot-August.png",
+  "./assets/images/Sapphire-September.png",
+  "./assets/images/Opal-October.png",
+  "./assets/images/Citrine-November.png",
+  "./assets/images/Tanzanite-December.png"
 ];
 let stoneArray = [];
 
 //FUNCTIONS
-$(document).ready(function () {
-    jRandom();
-    gRandom();
-
+$(document).ready(function() {
+  jRandom();
+  gRandom();
 });
 
 function jRandom() {
-    //create goal value for game
-    goal = Math.floor(Math.random() * 120 + 19);
-    //create the random values for the gems
-    for (let i = 0; i < 4; i++) {
-        let value = 0;
-        console.log(i);
-        randomValue();
-        function randomValue() {
-            value = Math.floor(Math.random() * birthstones.length);
-            console.log(value);
-            if (gemArray.includes(value)) {
-                randomValue();
-            } else {
-                console.log(value);
-                gemArray.push(value);            
-            }
-        }
+  //create goal value for game
+  goal = Math.floor(Math.random() * 120 + 19);
+  //create the random values for the gems
+  for (let i = 0; i < 4; i++) {
+    let value = 0;
+    console.log(i);
+    randomValue();
+    function randomValue() {
+      value = Math.floor(Math.random() * birthstones.length +1);
+      // console.log(value);
+      gemArray.includes(value) ? randomValue() : gemArray.push(value);
     }
+    let check = 0;
+    for (let i = 0; i < 4; i++) {
+      gemArray[i] % 2 === 0 ? check++ : console.log("odd");
+    }
+    check > 4 && check < 1
+      ? console.log("moo")
+      : function() {
+          gemArray = [];
+          jRandom();
+        };
+  }
 
-    //setting the gem values from array
-    gem1 = gemArray[0];
-    console.log("Gem Values: " + gemArray);
-    gem2 = gemArray[1];
-    gem3 = gemArray[2];
-    gem4 = gemArray[3];
-    //logging the numbers
-    logNumbers();
-    $('#gem1').val(gem1);
-    $('#gem2').attr("value", gem2);
-    $('#gem3').attr("value", gem3);
-    $('#gem4').attr("value", gem4);
-    $('#goal').html(goal);
-    console.log($('#gem1').val());
+  //setting the gem values from array
+  gem1 = gemArray[0];
+  console.log("Gem Values: " + gemArray);
+  gem2 = gemArray[1];
+  gem3 = gemArray[2];
+  gem4 = gemArray[3];
+  //logging the numbers
+  logNumbers();
+  $("#gem1").val(gem1);
+  $("#gem2").attr("value", gem2);
+  $("#gem3").attr("value", gem3);
+  $("#gem4").attr("value", gem4);
+  $("#goal").html(goal);
+  console.log($("#gem1").val());
 }
 //Image Functions of Games
-//generate images of the buttons 
+//generate images of the buttons
 function gRandom() {
-    for (let g = 0; g < 4; g++) {
-        let month = 0;
-        console.log("g value= " +g);
+  stoneArray=[];
+  for (let g = 0; g < 4; g++) {
+    let month = 0;
+    // console.log("g value= " + g);
+    randomMonth();
+    function randomMonth() {
+      month = Math.floor(Math.random() * birthstones.length);
+      // console.log(month);
+      if (stoneArray.includes(month)) {
         randomMonth();
-        function randomMonth() {
-            month = Math.floor(Math.random() * birthstones.length);
-            console.log(month);
-            if (stoneArray.includes(month)) {
-                randomMonth();
-            } else {
-                console.log(month);
-                stoneArray.push(month);
-            }
-        }
+      } else {
+        // console.log(month);
+        stoneArray.push(month);
+      }
     }
-    console.log("Stone Array: " + stoneArray);
-    //setting gem images to buttons
-    $('#gem1').css("background-image", "url(" + birthstones[stoneArray[0]] + ")");
-    $("#gem2").css("background-image", "url(" + birthstones[stoneArray[1]] + ")");
-    $("#gem3").css("background-image", "url(" + birthstones[stoneArray[2]] + ")");
-    $("#gem4").css("background-image", "url(" + birthstones[stoneArray[3]] + ")");
+  }
+  console.log("Stone Array: " + stoneArray);
+  //setting gem images to buttons
+  $("#gem1").css("background-image", "url(" + birthstones[stoneArray[0]] + ")");
+  $("#gem2").css("background-image", "url(" + birthstones[stoneArray[1]] + ")");
+  $("#gem3").css("background-image", "url(" + birthstones[stoneArray[2]] + ")");
+  $("#gem4").css("background-image", "url(" + birthstones[stoneArray[3]] + ")");
 }
-
-
 
 //updating the guess array with new variable
-$('.button').on('click', function () {
-    let value2 = parseInt($(this).val());
-    guess = parseInt(guess + value2);
-    console.log(guess)
-    console.log(typeof (value2));
-    $('#guess').html(guess);
-    runGame();
-}
-)
+$(".button").on("click", function() {
+  let value2 = parseInt($(this).val());
+  guess = parseInt(guess + value2);
+  // console.log(guess);
+  // console.log(typeof value2);
+  $("#guess").html(guess);
+  runGame();
+});
 
 //function for random number generator of gem values
 function randomNum() {
-    value = Math.floor(Math.random() * 12 + 1);
-    if (gemArray.includes(value)) {
-        randomNum();
-    }
+  value = Math.floor(Math.random() * 12 + 1);
+  if (gemArray.includes(value)) {
+    randomNum();
+  }
 }
-
-
 
 //creates a console.log to know gem values and win condition
 function logNumbers() {
-    let note = [gem1, gem2, gem3, gem4, goal];
-    console.log(note);
+  let note = [gem1, gem2, gem3, gem4, goal];
+  // console.log(note);
 }
-
 
 //Checking Win Conditions
 function runGame() {
-    if (guess > goal) {
-        losses++;
-        confirm('Loser!  Try Again')
-        $('#losses').html("Losses: " + losses);
-        reset();
-    }
-    else if (guess === goal) {
-        victory++;
-        alert('Congratulations You Have Won!')
-        $('#victory').html("Wins: " + victory);
-        reset();
-    }
+  if (guess > goal) {
+    losses++;
+    confirm("Loser!  Try Again");
+    $("#losses").html("Losses: " + losses);
+    reset();
+  } else if (guess === goal) {
+    victory++;
+    alert("Congratulations You Have Won!");
+    $("#victory").html("Wins: " + victory);
+    reset();
+  }
 }
 
 //Reset Game
 function reset() {
-    guess = 0;
-    $('#guess').html(guess);
-    gemArray=[];
-    jRandom();
-    gRandom();
+  guess = 0;
+  $("#guess").html(guess);
+  gemArray = [];
+  jRandom();
+  gRandom();
 }
 //EXCESSIVE FIRST ATTEMPTS (JUST FOR LOG/REF)
 
@@ -158,7 +156,7 @@ function reset() {
 //         randomNumberGenerator();
 //     } else
 //         if (gem2 === gem3 || gem2 === gem4) {
-    //             randomNumberGenerator();
+//             randomNumberGenerator();
 //         } else
 //             if (gem3 === gem4) {
 //                 randomNumberGenerator();
@@ -171,16 +169,16 @@ function reset() {
 // let values=[];
 // arr.filter(function(item)){
 //     if(!values.includes(item)){
-    //         values.push(item);
+//         values.push(item);
 //         return true;
 //     }
 // }
-    //Update Guess display
-    // // function updateGuessValue() {
-    //     for (let i = 0; i < guess.length; i++) {
-    //         let total = 0;
-    //         guess = total += i;
-    //         console.log(guess)
-    //         $('#guess').html(guess)
-    //     }
-    // }
+//Update Guess display
+// // function updateGuessValue() {
+//     for (let i = 0; i < guess.length; i++) {
+//         let total = 0;
+//         guess = total += i;
+//         console.log(guess)
+//         $('#guess').html(guess)
+//     }
+// }
